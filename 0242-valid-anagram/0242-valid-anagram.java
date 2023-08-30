c89ea22d9f4ffe70428ef1da3a20f136b5ecf80c
Eya-Helali <87916673+Eya-Helali@users.noreply.github.com>
2023-08-30 21:02:19 +0100

    Time: 3 ms (91.59%), Space: 43.8 MB (22.71%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,17 +1,20 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
+  String new_s= sorting(s);
+  String new_t= sorting(t);
 
-HashMap<Character, Integer> map= new HashMap();
-for(char c: s.toCharArray())
-{map.put(c, map.getOrDefault(c,0)+1);}
+ return new_s.equals(new_t);
 
-for(char x: t.toCharArray())
-{map.put(x,map.getOrDefault(x,0)-1);}
 
-for(int val : map.values())
-{if (val!=0)
-return false;}
-return true ;
-}
+
+
+    }
+
+      public String sorting(String l){
+        char[] stringArray=l.toCharArray();
+        Arrays.sort(stringArray);
+        return new String(stringArray);
+      }
+
 }
